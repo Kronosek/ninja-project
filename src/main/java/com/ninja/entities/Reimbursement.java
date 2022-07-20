c1f68a8ac0bee35e6344b49743a5460c618f7498
WinnerTakesAll <kronomail@protonmail.com>
2022-07-20 06:36:07 -0400

    new commit

--- src/main/java/com/ninja/entities/Reimbursement.java
+++ src/main/java/com/ninja/entities/Reimbursement.java
@@ -1,10 +1,10 @@
-package com.ninja;
+package com.ninja.entities;
 
 /**
  * Hello world!
  *
  */
-public class App 
+public class Reimbursement 
 {
     public static void main( String[] args )
     {
